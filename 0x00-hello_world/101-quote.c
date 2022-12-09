dcbc0d21fb6911c11f13d93fe827ac6e5fcb626c
Abdul-Qudus <akindelequdus2@gmail.com>
2022-12-08 20:22:49 -0800

    Qoutes

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - C program that prints some words
+ * code by Abdul-Qudus
+ * Return: 1
+*/
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
