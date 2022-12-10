e2d64822fdb4951990254c21d60c2f1965978e56
Abdul-Qudus <akindelequdus2@gmail.com>
2022-12-09 17:40:42 -0800

    Combination

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - program to print possible combinations
+ * Return: 0
+*/
+
+int main(void)
+{
+	int a;
+
+	for (a = 48; a < 58; a++)
+
+		putchar(n);
+		if (a != 57)
+		{
+			putchar(',');
+			putchar(',');
+		}
+	putchar('\n');
+	return (0);
+}
