bd100fb7440ed0546467c5021fc5cf9552e2df31
Abdul-Qudus <akindelequdus2@gmail.com>
2022-12-09 17:06:27 -0800

    lowercase reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print lowercase in reverse
+ * Return: 0
+*/
+
+int main(void)
+{
+	char lc;
+
+	for (lc = 'z'; lc >= a; lc--)
+		putchar(lc);
+
+	putchar('\n');
+
+	return (0);
+}
