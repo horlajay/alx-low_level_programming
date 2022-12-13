3e46f70c4c7ffd6a4e5fa455f8e4ed622760257a
Abdul-Qudus <akindelequdus2@gmail.com>
2022-12-13 14:26:44 -0800

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphabets in lowercase
+ * code by Qudus
+ * Return: 0
+*/
+
+void print_alphabet(void)
+{
+	char letters;
+
+	for (letters = 'a'; letters <= 'z'; letters++)
+
+		_putchar(letters);
+
+
+	_putchar('\n');
+}
