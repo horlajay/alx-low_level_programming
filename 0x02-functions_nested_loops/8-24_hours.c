19f35c34ca1bfaaeeb20acd0f650a80169f23954
Abdul-Qudus <akindelequdus2@gmail.com>
2022-12-13 16:21:39 -0800

    task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+/*
+ * File: 8-24_hours.c
+ * Auth: Akindele Abdul-Qudus
+ */
+
+#include "main.h"
+
+/**
+ * jack_bauer - prints evry minute of the day of Jack Bauer.
+ * Jack Bauer, starting from 00:00 to 23:59.
+ */
+
+void jack_bauer(void)
+{
+	int hour, minutes;
+
+	for (hour = 0; hour <= 23; hour++)
+	{
+		for (minutes = 0; minutes <= 59; minutes++)
+		{
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((minutes / 10) + '0');
+			_putchar((minutes % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
