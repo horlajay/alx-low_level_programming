8fdbb4784725df1c3e2b08b6807b28805c0d4d3c
Abdul-Qudus <akindelequdus2@gmail.com>
2022-12-16 13:50:41 -0800

    task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+/*
+ * File: 5-more_numbers.c
+ * Auth: Akindele Abdul-Qudus
+ */
+
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times the numbers from 0-14
+ */
+
+void more_numbers(void)
+{
+	int num, count;
+
+	for (count = 0; count <= 14; count++)
+	{
+		for (num = 0; num <= 14; num++)
+		{
+			if (num > 9)
+				_putchar((num / 10) + '0');
+			_putchar((num % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
