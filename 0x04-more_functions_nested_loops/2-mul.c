a415514a903ff6fd9b0cf82f4f03f47cdd6fc199
Abdul-Qudus <akindelequdus2@gmail.com>
2022-12-15 19:23:21 -0800

    Task 2 collaboration is multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+/*
+ * File: 2-mul.c
+ * Auth: Akindele Abdul-Qudus
+ */
+
+#include "main.h"
+
+/**
+ * mul - function that multiplies two integers.
+ * @a: The first integer
+ * @b: The second integer
+ * Return: The result of operation.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
