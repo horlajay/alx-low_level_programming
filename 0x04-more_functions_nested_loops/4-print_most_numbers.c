bb0c8548723171f93ce723057682e4c6fe2a16b6
Abdul-Qudus <akindelequdus2@gmail.com>
2022-12-16 13:32:17 -0800

    Task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -14,7 +14,11 @@ void print_most_numbers(void)
 	int a;
 
 	for (a = 0; a <= 9; a++)
-		if (a != 2 and a != 4)
+	{
+		if (a != 2 && a != 4)
+
 			_putchar((a % 10) + '0');
+	}
+
 	_putchar('\n');
 }
