98d5e5e1b1362f01dbf1369046eb14add9759737
Abdul-Qudus <akindelequdus2@gmail.com>
2022-12-15 19:32:06 -0800

    task 3 the numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,22 @@
+/*
+ * File: 3-print_numbers.c
+ * Auth: Akindele Abdul-Qudus
+ */
+
+#include "main.h"
+
+/**
+ * print_numbers - unction that prints the numbers, from 0 to 9
+ * followed by a new line.
+ */
+
+void print_numbers(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		_putchar(num);
+	}
+	_putchar('\n');
+}
