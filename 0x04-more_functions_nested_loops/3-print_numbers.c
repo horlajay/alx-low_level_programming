ad3c6059f0ca184f8d39107c30194c4912c0319d
Abdul-Qudus <akindelequdus2@gmail.com>
2022-12-15 19:39:48 -0800

    task 3 the numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,7 +6,7 @@
 #include "main.h"
 
 /**
- * print_numbers - unction that prints the numbers, from 0 to 9
+ * print_numbers - function that prints the numbers, from 0 to 9
  */
 
 void print_numbers(void)
