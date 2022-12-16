e80f71a66e1bb7411df559506e37a22a82aac79f
Abdul-Qudus <akindelequdus2@gmail.com>
2022-12-15 19:15:15 -0800

    task 1 isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,24 @@
+/*
+ * File: 1-isdigit.c
+ * Auth: Akindele Abdul-Qudus
+ */
+
+#include "main.h"
+
+/**
+ * _isdigit - function that checks for a digit (0 through 9)
+ * @c: The digit to be checked.
+ * Return: 1 if c is a digit, 0 otherwise.
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
