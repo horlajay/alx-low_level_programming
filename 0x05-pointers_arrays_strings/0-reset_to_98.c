5e40424574eb68cd1da07086e88f2085c4f754ba
Abdul-Qudus <akindelequdus2@gmail.com>
2022-12-19 13:49:01 -0800

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+/*
+ * File: 0-reset_to_98.c
+ * Auth: Akindele Abdul-Qudus
+ */
+
+#include "main.h"
+/**
+ * reset_to_98-Resets the value of a variable back to 98.
+ * @n:Variable to be reset.
+ * Return:Void.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
