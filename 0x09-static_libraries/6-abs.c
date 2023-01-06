aa8ef7ae69e4426b61bff0ad619730af7620f08a
Abdul-Qudus <akindelequdus2@gmail.com>
2023-01-06 13:46:09 -0800

    My Tasks

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an integer
+ * @a: The absolute value to compute
+ * Return: The absolute value of the integer
+ */
+
+int _abs(int a)
+{
+	if (a >= 0)
+		return (a);
+	else
+		return (-a);
+}
