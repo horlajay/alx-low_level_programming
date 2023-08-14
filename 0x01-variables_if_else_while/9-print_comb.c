874ee8b309f5d7e9992288bcfc85c99ca4eaa76c
Abdul-Qudus <akindelequdus2@gmail.com>
2023-08-14 02:10:36 -0700

    Task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'Prints combination of all single digits'
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
