1c70b3f664611c2676c9dbb5602d927e618892b0
Abdul-Qudus <akindelequdus2@gmail.com>
2023-08-14 01:31:30 -0700

    Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'prints the lowercase alphabet in reverse'
+ * Return: Always 0
+ */
+int main(void)
+{
+	char a;
+
+	for (n = 'z'; n >= 'a'; n--)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
