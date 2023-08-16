2ce0d14cf3cf99026762158fe7db788bd9b99363
horlajay <akindelequdus2@gmail.com>
2023-08-16 12:59:33 -0700

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,24 +7,21 @@
 */
 void print_alphabet_x10(void)
 {
-	char ch;
+	char c;
 	int i;
 
-
 	i = 0;
-
-        while (i < 10)
+	while (i < 10)
 	{
-	ch = 'a';
-	while (ch <= 'z')
-	{
-	_putchar(ch);
-	ch++;
-	}
-	_putchar('\n');
-	i++;
+		ch = 'a';
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+		_putchar('\n');
+		i++;
 	}
 }
-
 
 
