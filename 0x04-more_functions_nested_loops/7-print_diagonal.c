004feaea5c78e151bd7b043dc8d3ce3691fee211
horlajay <akindelequdus2@gmail.com>
2023-08-17 09:49:00 -0700

    Task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_line - draw a diagonal line
+ * @n: number of times underscore is printed
+ * Description: Can only use _putchar to print
+ */
+void print_diagonal(int n)
+{
+	int c;
+
+	for (c = 0; c < n; c++)
+	{
+		_putchar('\');
+	}
+	_putchar('\n');
+}
